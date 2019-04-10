f0bc40f65c69a914aa1a160c7f7daaff26adc6ab
Alfredo Hinarejos <alfredo.hina@gmail.com>
2019-04-10 17:27:55 +0200

    starting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  testApi() {
+    return axios.get("https://api.discogs.com/releases/249504").then(res => {
+      console.log(res.data)
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Search Artis, Album or both</p>
+          <button onClick={this.testApi}>
+            Launch API
+          </button>
         </header>
       </div>
     );
